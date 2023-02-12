98267bb17bb9c96440e45b9c2b6ef371bf2a900d
baphomet-disciple <122203558+baphomet-disciple@users.noreply.github.com>
2023-02-12 22:08:55 +0530

    Update zed.js

--- zed.js
+++ zed.js
@@ -2,10 +2,11 @@ const express = require('express');
 const { exec } = require('child_process');
 const app = express();
 
-app.get('/run?:website', (req, res) => {
+app.get('/run/:website', (req, res) => {
   const website = req.params.website;
+  console.log(`https://${website}`)
   exec(
-    `python3 dos.py ${website}`,
+    `python dos.py https://${website}`,
     (error, stdout, stderr) => {
       if (error) {
         res.status(500).send(error);
@@ -16,6 +17,6 @@ app.get('/run?:website', (req, res) => {
   );
 });
 
-app.listen(3001, () => {
+app.listen(3002, () => {
   console.log('API server running on port 3001');
 });
