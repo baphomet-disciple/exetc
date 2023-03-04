1e78fca70ded187546da2d375cf511aab007631b
baphomet-disciple <122203558+baphomet-disciple@users.noreply.github.com>
2023-03-04 21:26:12 +0530

    Create tls.js

---
+++ tls.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const { exec } = require('child_process');
+const app = express();
+
+app.get('/run/:website', (req, res) => {
+  const website = req.params.website;
+  console.log(`https://${website}`)
+  exec(
+    `python3 tls.py https://${website}`,
+    (error, stdout, stderr) => {
+      if (error) {
+        res.status(500).send(error);
+      } else {
+        res.send(stdout);
+      }
+    }
+  );
+});
+
+app.listen(3001, () => {
+  console.log('API server running on port 3001');
+});
